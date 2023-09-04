dfa0e103cb5961275c71697789d9a593cf3ca7a8
DaMing <haohwa2023@gmail.com>
2023-09-04 22:53:46 +0800

    add block to albums

--- main.js
+++ main.js
@@ -2,3 +2,13 @@ import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
 console.log("Hello world!");
+// 滾動條
+const swiper = new Swiper(".mySwiper", {
+    slidesPerView: "auto",
+    speed: 20000,
+    allowTouchMove: false,
+    loop: true,
+    autoplay: {
+      delay: 1
+    }
+  });
